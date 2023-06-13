47b2d5f5bb9beaae7bba7f533243d573486248fc
Wersacho <95137331+Wersacho@users.noreply.github.com>
2023-06-14 01:22:17 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -11,36 +11,36 @@ import { Recipe } from "./pages/recipe";
 
 
 function NoMatch() {
-  let location = useLocation();
+    let location = useLocation();
 
-  return (
-    <div>
-      <h1>Oops!</h1>
-      <h3>Sorry, an unexpected error has occurred.</h3>
-      <h5>Does not exist: {location.pathname}</h5>
-    </div>
-  );
+    return (
+        <div>
+            <h1>Oops!</h1>
+            <h3>Sorry, an unexpected error has occurred.</h3>
+            <h5>Does not exist: {location.pathname}</h5>
+        </div>
+    );
 };
 
 function App() {
-  return (
-    <>
-      <BrowserRouter>
-        <Header />
-        <main className="container content">
-          <Routes>
-            <Route path="*" element={<NoMatch />}></Route>
-            <Route path="/" element={<Home />} />
-            <Route path="contact/" element={<Contact />} />
-            <Route path="about/" element={<About />} />
-            <Route path="category/:name" element={<Category />} />
-            <Route path="meal/:id" element={<Recipe />} />
-          </Routes>
-        </main>
-        <Footer />
-      </BrowserRouter>
-    </>
-  );
+    return (
+        <>
+            <BrowserRouter basename="/spa-project">
+                <Header />
+                <main className="container content">
+                    <Routes>
+                        <Route path="*" element={<NoMatch />}></Route>
+                        <Route path="/" element={<Home />} />
+                        <Route path="contact/" element={<Contact />} />
+                        <Route path="about/" element={<About />} />
+                        <Route path="category/:name" element={<Category />} />
+                        <Route path="meal/:id" element={<Recipe />} />
+                    </Routes>
+                </main>
+                <Footer />
+            </BrowserRouter>
+        </>
+    );
 }
 
 export default App;
